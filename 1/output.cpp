88d6c9b2985a0d26acd58f6cae9b1e03edc7e566
dazzlemon <daanill13470@gmail.com>
2021-09-13 22:03:50 +0300

    removed some redundant cases, & added some more

--- 1/output.cpp
+++ 1/output.cpp
@@ -28,18 +28,35 @@ int main() {
   class3.method();
   class4.function();
 
-  // class3.function()
   function();
   ns::function;
 
-  // function
-  // .function
-  // .function()
+  // class3.function()
+  /* class3.function() */
+  /* class3.function()
+  */
+  /*
+  class3.function() */
+  /*
+  class3.function()
+  */
 
   class1.field.function;
   class2.field.function();
 
-  std::string some_string1 = "class3.function()";
-  std::srting some_string2("class2.function()");
-  char char_array[] = "class3.function()";
+  "class3.function()";
+  "
+  class3.function()";
+  "class3.function()
+  ";
+  "
+  class3.function();
+  ";
+
+  R"meme(
+      class3.function();
+  )meme";
+  R"""(
+      class3.function();
+  )""";
 }
