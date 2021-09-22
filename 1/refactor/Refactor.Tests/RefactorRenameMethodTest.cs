6072b784e5633aff5dbd89d7847b085f2b60cab9
dazzlemon <daanill13470@gmail.com>
2021-09-22 23:01:35 +0300

    1/refactor/Refactor.Tests/RefactorRenameMethodTest.cs reformat

--- 1/refactor/Refactor.Tests/RefactorRenameMethodTest.cs
+++ 1/refactor/Refactor.Tests/RefactorRenameMethodTest.cs
@@ -1,13 +1,10 @@
 using System;
 using Xunit;
 
-namespace Refactor.Tests
-{
-    public class RefactorRenameMethodTest
-    {
+namespace Refactor.Tests {
+    public class RefactorRenameMethodTest {
         [Fact] //1
-        public void Rename_SimplestCase()
-        {
+        public void Rename_SimplestCase() {
             var refactor = new Refactor();
             string result = refactor.RenameMethod("func", "method", "void func();");
             string expected = "void method();";
@@ -15,8 +12,7 @@ namespace Refactor.Tests
         }
 
         [Fact] //2
-        public void Rename_CaseWithSpacesBetweenBrackets()
-        {
+        public void Rename_CaseWithSpacesBetweenBrackets() {
             var refactor = new Refactor();
             string result = refactor.RenameMethod("func", "method", "void func     ();");
             string expected = "void method     ();";
@@ -24,18 +20,15 @@ namespace Refactor.Tests
         }
 
         [Fact] //3
-        public void Simplest_CaseWithParameter()
-        {
+        public void Simplest_CaseWithParameter() {
             var refactor = new Refactor();
             string result = refactor.RenameMethod("func", "method", "int func(int parameter);");
             string expected = "int method(int parameter);";
             Assert.Equal(expected, result);
-
         }
 
         [Fact] //4
-        public void NotRename_MethodNameInString()
-        {
+        public void NotRename_MethodNameInString() {
             var refactor = new Refactor();
             string result = refactor.RenameMethod("func", "method", "string s = \" void func();\"");
             string expected = "string s = \" void func();\"";
@@ -43,8 +36,7 @@ namespace Refactor.Tests
         }
 
         [Fact] //5
-        public void NotRename_MethodNameAsVariableName()
-        {
+        public void NotRename_MethodNameAsVariableName() {
             var refactor = new Refactor();
             string result = refactor.RenameMethod("func", "method", "int func = 0;");
             string expected = "int func = 0;";
@@ -52,28 +44,23 @@ namespace Refactor.Tests
         }
 
         [Fact] //6
-        public void NotRename_MethodNameInSingleLineComment()
-        {
+        public void NotRename_MethodNameInSingleLineComment() {
             var refactor = new Refactor();
             string result = refactor.RenameMethod("func", "method", "//void func();");
             string expected = "//void func();";
             Assert.Equal(expected, result);
-
         }
 
         [Fact] //7
-        public void NotRename_MethodNameInMultiLineComment()
-        {
+        public void NotRename_MethodNameInMultiLineComment() {
             var refactor = new Refactor();
             string result = refactor.RenameMethod("func", "method", "/*void func();*/");
             string expected = "/*void func();*/";
             Assert.Equal(expected, result);
-
         }
 
         [Fact] //8
-        public void NotRename_FamiliarMethodNamePost()
-        {
+        public void NotRename_FamiliarMethodNamePost() {
             var refactor = new Refactor();
             string result = refactor.RenameMethod("func", "method", "void func2();");
             string expected = "void func2();";
@@ -81,18 +68,15 @@ namespace Refactor.Tests
         }
 
         [Fact] //9
-        public void NotRename_FamiliarMethodNamePref()
-        {
+        public void NotRename_FamiliarMethodNamePref() {
             var refactor = new Refactor();
             string result = refactor.RenameMethod("func", "method", "void some_func();");
             string expected = "void some_func();";
             Assert.Equal(expected, result);
-
         }
 
         [Fact] //10
-        public void Rename_MethodNameInDifferentObjects()
-        {
+        public void Rename_MethodNameInDifferentObjects() {
             var refactor = new Refactor();
             string codeText =
                    @" 
@@ -101,7 +85,7 @@ namespace Refactor.Tests
                       objectName1.func();
                       objectName2.func();
                     ";
-            string result = refactor.RenameMethod("func", "method", CodeText);
+            string result = refactor.RenameMethod("func", "method", codeText);
             string expected =
                    @" 
                       className1 objectName1;
@@ -113,8 +97,7 @@ namespace Refactor.Tests
         }
 
         [Fact] //11
-        public void Rename_MethodNameInPointerToFunction()
-        {
+        public void Rename_MethodNameInPointerToFunction() {
             var refactor = new Refactor();
             string CodeText =
                    @"
@@ -131,8 +114,7 @@ namespace Refactor.Tests
         }
 
         [Fact] //12
-        public void Rename_ComplexCase1()
-        {
+        public void Rename_ComplexCase1() {
             var refactor = new Refactor();
             string CodeText =
                    @"
@@ -155,8 +137,7 @@ namespace Refactor.Tests
         }
 
         [Fact] //13
-        public void Rename_ComplexCase2()
-        {
+        public void Rename_ComplexCase2() {
             var refactor = new Refactor();
             string CodeText =
                    @" 
