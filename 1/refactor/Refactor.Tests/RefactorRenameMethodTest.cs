13477c9d6c4753918d64548afca4b8ed6b8f0c4b
dazzlemon <daanill13470@gmail.com>
2021-09-23 15:50:15 +0300

    refactored RefactorRenameTest to [Theory]

--- 1/refactor/Refactor.Tests/RefactorRenameMethodTest.cs
+++ 1/refactor/Refactor.Tests/RefactorRenameMethodTest.cs
@@ -7,14 +7,64 @@ namespace Refactor.Tests {
     public class TestDataGenerator : IEnumerable<object[]> {
         private readonly List<object[]> _data = new List<object[]> {
             //{oldName, newName, input, expected}// TODO: put in class?
-            new object[] {"func", "method", "void func();", "void method();"},// 1
-            new object[] {"func", "method", "void func ();", "void method ();"},// 2
+            new object[] {"func", "method", "void func();", "void method();"},
+            new object[] {"func", "method", "void func ();", "void method ();"},
+            new object[] {"func", "method", "int func(int parameter);", "int method(int parameter);"},
+            new object[] {"func", "method", "string s = \" void func();\"", "string s = \" void func();\""},
+            new object[] {"func", "method", "int func = 0;", "int func = 0;"},
+            new object[] {"func", "method", "//void func();", "//void func();"},
+            new object[] {"func", "method", "/*void func();*/", "/*void func();*/"},
+            new object[] {"func", "method", "void func2();", "void func2();"},
+            new object[] {"func", "method", "void some_func();", "void some_func();"},
+            new object[] {"func", "method",
+@"className1 objectName1;
+className2 objectName2;
+objectName1.func();
+objectName2.func();"
+,
+@"className1 objectName1;
+className2 objectName2;
+objectName1.method();
+objectName2.method();"
+            },
+            new object[] {"func", "method",
+@"void func ( void (*f)(int) );
+void func2 ( void (*func)(int) );"
+,
+@"void method ( void (*f)(int) );
+void func2 ( void (*method)(int) );"
+            },
+            new object[] {"func", "method",
+@"// void func(int parameter) some text
+void func(int parameter) {
+    std::cout << ""void func() has "" << parameter << std::endl;
+}"
+,
+@"// void func(int parameter) some text
+void method(int parameter) {
+    std::cout << ""void func() has "" << parameter << std::endl;
+}"
+            },
+            new object[] {"func", "method",
+@"// void func(int parameter) some text
+void func2(int parameter) {
+    className objectName;
+    int randomNumber1 = func();
+    int randomNumber2 = objectName.func();
+}"
+,
+@"// void func(int parameter) some text
+void func2(int parameter) {
+    className objectName;
+    int randomNumber1 = method();
+    int randomNumber2 = objectName.method();
+}"
+            },
         };
 
         public IEnumerator<object[]> GetEnumerator() => _data.GetEnumerator();
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
     }
-
 
     public class RefactorRenameMethodTest {
         [Theory]
@@ -25,137 +75,5 @@ namespace Refactor.Tests {
             string output = refactor.RenameMethod(oldName, newName, input);
             Assert.Equal(output, expected);
         }
-
-//         [Fact] //2
-//         public void Rename_CaseWithSpacesBetweenBrackets() {
-//             var refactor = new Refactor();
-//             string result = refactor.RenameMethod("func", "method", "void func ();");
-//             string expected = "void method ();";
-//             Assert.Equal(expected, result);
-//         }
-
-//         [Fact] //3
-//         public void Simplest_CaseWithParameter() {
-//             var refactor = new Refactor();
-//             string result = refactor.RenameMethod("func", "method", "int func(int parameter);");
-//             string expected = "int method(int parameter);";
-//             Assert.Equal(expected, result);
-//         }
-
-//         [Fact] //4
-//         public void NotRename_MethodNameInString() {
-//             var refactor = new Refactor();
-//             string result = refactor.RenameMethod("func", "method", "string s = \" void func();\"");
-//             string expected = "string s = \" void func();\"";
-//             Assert.Equal(expected, result);
-//         }
-
-//         [Fact] //5
-//         public void NotRename_MethodNameAsVariableName() {
-//             var refactor = new Refactor();
-//             string result = refactor.RenameMethod("func", "method", "int func = 0;");
-//             string expected = "int func = 0;";
-//             Assert.Equal(expected, result);
-//         }
-
-//         [Fact] //6
-//         public void NotRename_MethodNameInSingleLineComment() {
-//             var refactor = new Refactor();
-//             string result = refactor.RenameMethod("func", "method", "//void func();");
-//             string expected = "//void func();";
-//             Assert.Equal(expected, result);
-//         }
-
-//         [Fact] //7
-//         public void NotRename_MethodNameInMultiLineComment() {
-//             var refactor = new Refactor();
-//             string result = refactor.RenameMethod("func", "method", "/*void func();*/");
-//             string expected = "/*void func();*/";
-//             Assert.Equal(expected, result);
-//         }
-
-//         [Fact] //8
-//         public void NotRename_FamiliarMethodNamePost() {
-//             var refactor = new Refactor();
-//             string result = refactor.RenameMethod("func", "method", "void func2();");
-//             string expected = "void func2();";
-//             Assert.Equal(expected, result);
-//         }
-
-//         [Fact] //9
-//         public void NotRename_FamiliarMethodNamePref() {
-//             var refactor = new Refactor();
-//             string result = refactor.RenameMethod("func", "method", "void some_func();");
-//             string expected = "void some_func();";
-//             Assert.Equal(expected, result);
-//         }
-
-//         [Fact] //10
-//         public void Rename_MethodNameInDifferentObjects() {
-//             var refactor = new Refactor();
-//             string codeText =
-// @"className1 objectName1;
-// className2 objectName2;
-// objectName1.func();
-// objectName2.func();";
-//             string result = refactor.RenameMethod("func", "method", codeText);
-//             string expected =
-// @"className1 objectName1;
-// className2 objectName2;
-// objectName1.method();
-// objectName2.method();";
-//             Assert.Equal(expected, result);
-//         }
-
-//         [Fact] //11
-//         public void Rename_MethodNameInPointerToFunction() {
-//             var refactor = new Refactor();
-//             string CodeText =
-// @"void func ( void (*f)(int) );
-// void func2 ( void (*func)(int) );";
-//             string result = refactor.RenameMethod("func", "method", CodeText);
-//             string expected =
-// @"void method ( void (*f)(int) );
-// void func2 ( void (*method)(int) );";
-//             Assert.Equal(expected, result);
-//         }
-
-//         [Fact] //12
-//         public void Rename_ComplexCase1() {
-//             var refactor = new Refactor();
-//             string CodeText =
-// @"// void func(int parameter) some text
-// void func(int parameter) {
-//     std::cout << ""void func() has "" << parameter << std::endl;
-// }";
-//             string result = refactor.RenameMethod("func", "method", CodeText);
-//             string expected =
-// @"// void func(int parameter) some text
-// void method(int parameter) {
-//     std::cout << ""void func() has "" << parameter << std::endl;
-// }";
-//             Assert.Equal(expected, result);
-//         }
-
-//         [Fact] //13
-//         public void Rename_ComplexCase2() {
-//             var refactor = new Refactor();
-//             string CodeText =
-// @"// void func(int parameter) some text
-// void func2(int parameter) {
-//     className objectName;
-//     int randomNumber1 = func();
-//     int randomNumber2 = objectName.func();
-// }";
-//             string result = refactor.RenameMethod("func", "method", CodeText);
-//             string expected =
-// @"// void func(int parameter) some text
-// void func2(int parameter) {
-//     className objectName;
-//     int randomNumber1 = method();
-//     int randomNumber2 = objectName.method();
-// }";
-//             Assert.Equal(expected, result);
-//         }
     }
 }
